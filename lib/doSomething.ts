819e2b02de34f0542db0a71e85dd58848e2416f2
k22036 <k22036kk@aitech.ac.jp>
2026-01-02 21:52:10 +0900

    feat: resolveAliasによるモック

--- lib/doSomething.ts
+++ lib/doSomething.ts
@@ -1,4 +1,4 @@
-import { fetchSomeData } from "./fetcher";
+import { fetchSomeData } from "@/lib/fetcher";
 
 export const doSomething = (): string => {
   const response = fetchSomeData();
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: process.env.USE_MOCK
+    ? {
+        resolveAlias: {
+          "@/lib/fetcher": "@/lib/fetcher.mock",
+        },
+      }
+    : {},
 };
 
 export default nextConfig;
